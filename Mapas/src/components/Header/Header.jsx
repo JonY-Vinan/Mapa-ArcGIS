20cfac40b9a30106ab3d2eafde70fa0d02e18ac2
JonY-Vinan <jony.v.c.ecuaor95@gmail.com>
2025-03-03 22:37:34 +0100

    Projecto base sin codigo.

---
+++ Mapas/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Header.css"; // Importamos el archivo CSS
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="navbar">
+        <a >
+          <i>Mapa</i>
+        </a>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
